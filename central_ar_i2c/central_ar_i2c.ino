1171e571adf494b4b73a8742acb192f96920adcc
Alessandro Vinicius Siquiera Santos <alessandro.vncs@gmail.com>
2019-11-11 08:34:03 -0300

    adicionado a função de tolerancia a falha

--- central_ar_i2c/central_ar_i2c.ino
+++ central_ar_i2c/central_ar_i2c.ino
@@ -12,11 +12,11 @@ const char* psw_development = "rm81589636";
 
 const char* version = "v1";
 
-const bool ledsEnabled = false;
-const bool sensorEnabled = false;
+const bool ledsEnabled = true;
+const bool sensorEnabled = true;
 const bool sendJson = true;
 const bool network = true;
-const bool testLocal = true;
+const bool testLocal = false;
 
 const int pinLeds[] = {1,3};
 const String fingerprint = "08:3B:71:72:02:43:6E:CA:ED:42:86:93:BA:7E:DF:81:C4:BC:62:30";
@@ -29,11 +29,14 @@ String iot_url_send =  "sensors";
 
 String token = "";
 String statusMode = "";
-int sendTime = 5; //Segundos
+int sendTime = 5000; //Segundos
 int timeSleep = 60; //Minutos
 
 const int filterSize = 10;
 int filter[filterSize];
+
+const int valueReset = 100;
+int contReset = 0;
 
 void ledShow(int id, int size, int speed){
 	digitalWrite(pinLeds[id], HIGH);
@@ -70,6 +73,9 @@ void ledShowAlter(int size, int speed){
 
 void setup() {
 
+	ESP.wdtDisable();
+	ESP.wdtEnable(WDTO_8S);
+
 	base_url = (testLocal ? localAddress + base_url : remoteAddress + base_url);
 	iot_url_send = base_url + iot_url_send;
 	iot_url_token = base_url + iot_url_token;
@@ -90,72 +96,37 @@ void setup() {
 	WiFi.mode(WIFI_STA);
 
 	if(!ledsEnabled) Serial.println(WiFi.macAddress().c_str());
-	while(!ccs.begin()){	
-		if(!sensorEnabled) break;
-		if(ledsEnabled){ 
-			ledShowAlter(10, 350);
-		} else {
-			Serial.println("Acessando sensor");
-			delay(1000);
-		}
-	}	
-	//CCS811_DRIVE_MODE_1SEC
-	ccs.setDriveMode(CCS811_DRIVE_MODE_250MS);
+	if(sensorEnabled) {
+		while(!ccs.begin()){	
+			if(ledsEnabled){ 
+				ledShowAlter(10, 350);
+			} else {
+				Serial.println("Acessando sensor");
+				delay(1000);
+			}
+		}	
+		//CCS811_DRIVE_MODE_250MS
+		ccs.setDriveMode(CCS811_DRIVE_MODE_250MS);
 
-	int cont = 0;
-	do{
-		if(ccs.available() && !ccs.readData() && ccs.geteCO2() > 0) {
-			filterPush(ccs.geteCO2());
-			cont++;
-		}
-		ledShow(1, 4, 80);
-	}while(cont < filterSize-1);
-}
-
-void modeSleep(int time) {
-	digitalWrite(pinLeds[0], LOW);
-	digitalWrite(pinLeds[1], LOW);
-	ccs.setDriveMode(CCS811_DRIVE_MODE_IDLE);
-	WiFi.forceSleepBegin(0);
-	delay(time);
-	WiFi.forceSleepWake();
-	ccs.setDriveMode(CCS811_DRIVE_MODE_250MS);
-	digitalWrite(pinLeds[0], HIGH);
-}
-
-void getSettings() {
-	String tmp = iot_url_token;
-	tmp.concat(WiFi.macAddress().c_str());
-	do{
-		httpRequest(tmp, "", "get");//Efetua a requisição no servidor	
-		if(WiFi.status() != WL_CONNECTED){
-			break;
-		}
-
-		if(statusMode == "paused") {
-			modeSleep(timeSleep);
-		}
-		delay(500);
-	}while(statusMode != "ok");		
-}
-
-void filterPush(int value) {
-	for(int i = 0; i < filterSize; i++) {
-		filter[i] = filter[i+1];
+		int cont = 0;
+		do{
+			if(ccs.available() && !ccs.readData() && ccs.geteCO2() > 0) {
+				filterPush(ccs.geteCO2());
+				cont++;
+			}
+			if(ledsEnabled) {
+				ledShow(1, 4, 80);
+			} else {
+				Serial.print("Recebendo valor: ");
+				Serial.print(cont);
+				Serial.println(" do sensor!");
+				delay(320);
+			}
+		}while(cont < filterSize-1);
 	}
-	filter[filterSize-1] = value;
 }
 
-int getFilter() {
-	int max = 0;
-	for(int i = 0; i < filterSize; i++) {
-		max += filter[i];
-	}
-
-	return max / filterSize;
-}
-
-void httpRequest(String url, char JSONmessageBuffer[300], String method){
+void httpRequest(String url,String method, char JSONmessageBuffer[] = ""){
 	HTTPClient http;
 
 	testLocal ? http.begin(url) : http.begin(url, fingerprint);
@@ -163,6 +134,7 @@ void httpRequest(String url, char JSONmessageBuffer[300], String method){
 	http.addHeader("Content-Type", "application/json");
 
 	int httpCode = 0;
+	//ESP.wdtDisable();
 	if(method == "post"){
 		http.addHeader("authentication", "Bearer " + token);
 		httpCode = http.POST(JSONmessageBuffer); 	
@@ -183,7 +155,7 @@ void httpRequest(String url, char JSONmessageBuffer[300], String method){
 		statusMode = doc["statusMode"].as<String>(); 
 		token = doc["token"].as<String>(); 
 		sendTime = doc["sendTime"].as<int>() * 1000;
-		timeSleep = doc["timeSleep"].as<int>() * 60000;
+		timeSleep = doc["timeSleep"].as<int>() * 60000 + (2 * sendTime);
 
 		if(!ledsEnabled){
 			Serial.println(statusMode);
@@ -206,7 +178,66 @@ void httpRequest(String url, char JSONmessageBuffer[300], String method){
 	http.end(); 
 }
 
+void modeSleep(int time) {
+	
+	if(ledsEnabled){
+		digitalWrite(pinLeds[0], LOW);
+		digitalWrite(pinLeds[1], LOW);
+	} else {
+		Serial.println("Mode Sleep: Actived");
+	}
+	ccs.setDriveMode(CCS811_DRIVE_MODE_IDLE);
+	WiFi.forceSleepBegin(0);
+	delay(time);
+	WiFi.forceSleepWake();
+	ccs.setDriveMode(CCS811_DRIVE_MODE_250MS);
+	if(ledsEnabled) {
+		digitalWrite(pinLeds[0], HIGH);
+	} else {
+		Serial.println("Mode Sleep: Desactived");
+	}
+}
+
+void getSettings() {
+	String tmp = iot_url_token;
+	tmp.concat(WiFi.macAddress().c_str());
+	int cont = 0;
+	do{
+		httpRequest(tmp, "get");//Efetua a requisição no servidor	
+		if(WiFi.status() != WL_CONNECTED){
+			break;
+		}
+
+		if(statusMode == "paused") {
+			modeSleep(timeSleep);
+		}
+		delay(100);
+		cont++;
+		if(cont > 10) {
+			modeSleep(sendTime);
+			ESP.reset();
+		}
+	}while(statusMode != "ok");		
+}
+
+void filterPush(int value) {
+	for(int i = 0; i < filterSize; i++) {
+		filter[i] = filter[i+1];
+	}
+	filter[filterSize-1] = value;
+}
+
+int getFilter() {
+	int max = 0;
+	for(int i = 0; i < filterSize; i++) {
+		max += filter[i];
+	}
+
+	return max / filterSize;
+}
+
 void checkNet(){
+	if(!ledsEnabled) Serial.println("Verificando internet");
 	if(WiFi.status() != WL_CONNECTED){ //Verifica conexão com a internet
 		if(!ledsEnabled){
 			Serial.println();
@@ -214,10 +245,15 @@ void checkNet(){
 			Serial.print("MAC: ");
 			Serial.println(WiFi.macAddress().c_str());
 		}
+
 		int attemptsCount = 0;
-	  	do{
+	  	do{	
 			WiFi.begin((testLocal) ? ssid_development : ssid_production, (testLocal) ? psw_development : psw_production);
-			for(int j = 0; j < 100; j++) {
+			/* if(contReset == 0) {
+			} else {
+				WiFi.reconnect();
+			} */
+			for(int j = 0; j < 10; j++) {
 				if(WiFi.status() == WL_CONNECTED){
 					break;
 				} else if(!ledsEnabled){
@@ -228,10 +264,9 @@ void checkNet(){
 				}
 			}
 			attemptsCount++;
-			if(attemptsCount ==  10){
-				WiFi.forceSleepBegin();
-				delay(15 * 1000);
-				ESP.restart();
+			if(attemptsCount > 4){
+				modeSleep(15*1000);
+				ESP.reset();
 			}
 	  	}while(WiFi.status() != WL_CONNECTED);
 
@@ -247,7 +282,9 @@ void checkNet(){
 }
 
 void loop() {
-	
+	contReset++;
+	ESP.wdtFeed();
+
 	if(network){
 		checkNet();//Verifica a conexão com a internet
 	}
@@ -256,7 +293,7 @@ void loop() {
 	 	getSettings();
 	 }
 	
-	if(sensorEnabled){
+	if(sensorEnabled && statusMode == "ok"){
 		if(ccs.available() && !ccs.readData()){
 			
 			filterPush(ccs.geteCO2());
@@ -265,7 +302,6 @@ void loop() {
 			DynamicJsonDocument JSONencoder(500);	
 			char JSONmessageBuffer[500] = "";
 
-			//JSONencoder["token"] = "Bearer " + token;
 			JsonArray nested = JSONencoder.createNestedArray("sensorData");
 			JsonObject after[1] = nested.createNestedObject();
 			after[0]["type"] = "co2";
@@ -280,7 +316,7 @@ void loop() {
 		
 
 			if(sendJson && statusMode == "ok"){
-				httpRequest(iot_url_send, JSONmessageBuffer, "post");//Envia os dados para o servidor
+				httpRequest(iot_url_send, "post", JSONmessageBuffer);//Envia os dados para o servidor
 			}
 				
 		} else {
@@ -292,7 +328,15 @@ void loop() {
 		}
 	}
 
-	if(!ledsEnabled) Serial.println(ESP.getVcc());
+	if(!ledsEnabled){
+		Serial.println(ESP.getVcc());
+		Serial.println(contReset);
+	} 
 
 	modeSleep(sendTime);
+	delay(0);
+
+	if(contReset > valueReset) {
+		ESP.reset();
+	}
 }
